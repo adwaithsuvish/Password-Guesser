8210d5046f78511cfebe1d3501fc22ba48a7eb40
Adwaith Suvish <adwaithsuvi@gmail.com>
2026-06-22 01:16:53 +0400

    Added more permutation

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ number_special = input("Enter a special number: ")
 special_characters = "~!@#$%^&*()-_=+[]{}|;:',.<>/?" + '"'
 
 passwords = list(permutation.permutation(name.split(), [dob[6:]], list(special_characters)))
+passwords += list(permutation.permutation(name.split(), list(special_characters), [dob[6:]]))    
+passwords += list(permutation.permutation(name.split(),list(special_characters), [dob[0:4]]))    
 
 f = open(f"{name.split()[0]}_passwords.txt", "w")
 f.writelines ("\n".join(passwords))
