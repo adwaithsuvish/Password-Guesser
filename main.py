e7a5a6262507b43bbefa99afe56f5dd14e456f44
Adwaith Suvish <adwaithsuvi@gmail.com>
2026-06-22 20:41:16 +0400

    Added more password possibilities

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ special_characters = "~!@#$%^&*()-_=+[]{}|;:',.<>/?" + '"'
 passwords = list(permutation.permutation(name.split(), [dob[6:]], list(special_characters)))
 passwords += list(permutation.permutation(name.split(), list(special_characters), [dob[6:]]))    
 passwords += list(permutation.permutation(name.split(),list(special_characters), [dob[0:4]]))    
+passwords += list(permutation.permutation(name.split(), list(special_characters), [number_special]))
+
 
 f = open(f"{name.split()[0]}_passwords.txt", "w")
 f.writelines ("\n".join(passwords))
